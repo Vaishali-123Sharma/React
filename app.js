2f45196fe0129784ffd8aa420280351faa1254c7
Vaishali <vaishalib326@gmail.com>
2024-09-20 16:53:53 +0530

    Created React First Project

---
+++ app.js
@@ -0,0 +1,5 @@
+const heading1 = React.createElement("h1", { id: "heading1" }, "Namaste Everyone");
+const heading2 = React.createElement("h1", { id: "heading2" }, "From Vaishali");
+const container = React.createElement("div", { id: "container" }, [heading1, heading2]);
+const root = ReactDOM.createRoot((document.getElementById("root")));
+root.render(container);
